d9ae9da465ce1d0567535c74351374466661d1c1
leeyouna21 <yuna5845@gmail.com>
2024-05-10 10:07:25 +0900

    first

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const Main = () => {
+  return (
+    <div className="main">ddss</div>
+  )
+}
+
+export default Main
